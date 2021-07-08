bdc81baca749c4b36e62a8d0aeaa287d383c7684
Desy Kristianti <desy.kristianti@oaknorth.ai>
2021-07-08 13:02:41 +0100

    Fix TextBlock css for printing

--- src/components/TextBlock/TextBlock.js
+++ src/components/TextBlock/TextBlock.js
@@ -1,10 +1,12 @@
+import cn from 'classnames';
 import { EditableText } from '@blueprintjs/core';
 
 import styles from './TextBlock.module.scss';
+import printStyles from './TextBlock.print.module.scss';
 
 const TextBlock = () =>
   <EditableText
-    className={styles.editableText}
+    className={cn(styles.editableText, printStyles.editableText)}
     multiline
     minLines={3}
     placeholder='Insert text here...'
